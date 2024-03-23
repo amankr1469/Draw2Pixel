7b694cde5c85411fc5ebf0ffd1af954f7f2af1fd
Aman <akjha1469@gmail.com>
2024-03-23 17:28:09 +0530

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,162 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from 'react'
+import * as fal from "@fal-ai/serverless-client"
+import Image from 'next/image'
 
+fal.config({
+  proxyUrl: "/fal/proxy",
+})
+
+const seed = Math.floor(Math.random() * 100000)
+const baseArgs = {
+  sync_mode: true,
+  strength: .99,
+  seed
+}
 export default function Home() {
+  const [input, setInput] = useState('A cozy cabin nestled in a snow-covered mountain range under a starry night sky')
+  const [image, setImage] = useState(null)
+  const [sceneData, setSceneData] = useState<any>(null)
+  const [excalidrawAPI, setExcalidrawAPI] = useState<any>(null)
+  const [_appState, setAppState] = useState<any>(null)
+  const [excalidrawExportFns, setexcalidrawExportFns] = useState<any>(null)
+  const [isClient, setIsClient] = useState<boolean>(false)
+  const [downloadEnabled, setDownloadEnabled] = useState<boolean>(false);
+
+  const [Comp, setComp] = useState<any>(null);
+  useEffect(() => {
+    import('@excalidraw/excalidraw').then((comp) => setComp(comp.Excalidraw))
+  }, [])
+  useEffect(() => { setIsClient(true) }, [])
+  useEffect(() => {
+    import('@excalidraw/excalidraw').then((module) =>
+      setexcalidrawExportFns({
+        exportToBlob: module.exportToBlob,
+        serializeAsJSON: module.serializeAsJSON
+      })
+    );
+  }, []);
+
+  const { send } = fal.realtime.connect('110602490-sdxl-turbo-realtime', {
+    connectionKey: 'draw-2-pixel',
+    onResult(result) {
+      if (result.error) return
+      setImage(result.images[0].url)
+      setDownloadEnabled(true);
+    }
+  })
+
+  async function getDataUrl(appState = _appState) {
+    const elements = excalidrawAPI.getSceneElements()
+    if (!elements || !elements.length) return
+    const blob = await excalidrawExportFns.exportToBlob({
+      elements,
+      exportPadding: 0,
+      appState,
+      quality: 0.5,
+      files: excalidrawAPI.getFiles(),
+      getDimensions: () => { return {width: 450, height: 450}}
+    })
+    return await new Promise(r => {let a=new FileReader(); a.onload=r; a.readAsDataURL(blob)}).then((e:any) => e.target.result)
+  }
+ 
+  const handleImageDownload = () => {
+    if (image) {
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'fal_image.png';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-3">
+      <div className='flex flex-col mb-4 justify-center items-center '>
+      <p className="mb-2 font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Draw 2 Pixel</p>
+      <p className='font-mono text-2xl flex justify-center'>Enter the prompt and draw to generate image</p>
+      </div>
+      <input
+        className='border rounded-lg p-2 w-full text-black'
+        value={input}
+        onChange={async (e) => {
+          setInput(e.target.value)
+          let dataUrl = await getDataUrl()
+          send({
+            ...baseArgs,
+            prompt: e.target.value,
+            image_url: dataUrl
+          })
+        }}
+      />
+
+      <div className='flex flex-col md:flex-row items-center justify-center'>
+
+        <div className="w-[550px] h-[570px] m-10">
+          {
+            isClient && excalidrawExportFns && (
+              <Comp
+                excalidrawAPI={(api: any)=> setExcalidrawAPI(api)}
+                onChange={async (elements: any, appState: any) => {
+                  const newSceneData = excalidrawExportFns.serializeAsJSON(
+                    elements,
+                    appState,
+                    excalidrawAPI.getFiles(),
+                    'local'
+                  )
+                  if (newSceneData !== sceneData) {
+                    setAppState(appState)
+                    setSceneData(newSceneData)
+                    let dataUrl = await getDataUrl(appState)
+                    send({
+                      ...baseArgs,
+                      image_url: dataUrl,
+                      prompt: input,
+                    })
+                  }
+                }}
+              />
+            )
+          }
         </div>
-      </div>
+        <div></div>
+        {image && (
+          <div className='flex flex-col p-8'>
+            <Image
+              src={image}
+              width={500}
+              height={500}
+              alt='fal image'
+            />
+            <br />
+            {downloadEnabled && (
+              <button
+                className="top-30 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+                onClick={handleImageDownload}
+              >
+                Download Image
+              </button>
+            )}
+          </div>
+        )}
+        <div className="fixed bottom-10 right-10 group">
+    <a href="https://github.com/amankr1469/Draw2Pixel" target='blank'>
+        <button>
+            <svg stroke-linejoin="round" stroke-linecap="round" stroke-width="2" stroke="currentColor" fill="none" viewBox="0 0 24 24" className="w-8 hover:scale-125 duration-200 hover:stroke-pink-500">
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </svg>
+        </button>
+    </a>
+    <span className="absolute -top-14 left-[50%] -translate-x-[50%] 
+    z-20 origin-left scale-0 px-3 rounded-lg border 
+    border-gray-300 bg-white py-2 text-sm font-bold
+    shadow-md transition-all duration-300 ease-in-out 
+    group-hover:scale-100 text-black">Give⭐<span>
+    </span>
+    </span>
+</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
